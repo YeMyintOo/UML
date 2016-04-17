cd8c24552023af9f58643eac2a1490d295eaf485
Ye Myint Oo <dynamic.mdy.95@gmail.com>
2016-04-17 14:33:53 +0630

    IO: Project Folder and Properties XML File creation is enabled

--- src/Boxes/Box_NPro.java
+++ src/Boxes/Box_NPro.java
@@ -1,8 +1,13 @@
 package Boxes;
 
+import java.io.File;
+import java.io.IOException;
+
 import Database.SystemHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -34,10 +39,10 @@ public class Box_NPro extends Stage {
 		initModality(Modality.WINDOW_MODAL); // Prevent click parent stage
 		initOwner(owner);
 		setResizable(false);
-		if(systemHandler==null){
-			systemHandler=new SystemHandler();
+		if (systemHandler == null) {
+			systemHandler = new SystemHandler();
 		}
-		
+
 		value = "default";
 		setTitle("New Project");
 		BorderPane pane = new BorderPane();
@@ -96,14 +101,43 @@ public class Box_NPro extends Stage {
 		});
 		okB.setOnAction(e -> {
 			// Create Project
-			setValue("finish");
-			close();
+			// Create Folder
+			if (!nameF.getText().equals("")) {
+				File file = new File(systemHandler.getDefaultWorkspace() + "\\" + nameF.getText());
+				try {
+					if (!file.exists()) {
+						file.mkdirs(); // Make directory
+						System.out.println("Project Folder is created");
+						createProjectProperties(file.getAbsolutePath());
+						setValue("finish");
+						close();
+					} else {
+						// File exist
+						Alert alert = new Alert(AlertType.ERROR);
+						alert.initModality(Modality.APPLICATION_MODAL);
+						alert.initOwner(this);
+						alert.setTitle("Error");
+						alert.setHeaderText("File alwary exits");
+						alert.setContentText("Choose different project name");
+						alert.showAndWait();
+					}
+				} catch (Exception ee) {
+					ee.printStackTrace();
+				}
+
+			}
+
 		});
 		wsB.setOnAction(e -> {
-			
+
 		});
 
-		
+	}
+
+	public void createProjectProperties(String path) throws IOException {
+		File file = new File(path+"\\properties.xml");
+		file.createNewFile();
+		System.out.println("Create Project Properties File");
 	}
 
 	public void setValue(String value) {
