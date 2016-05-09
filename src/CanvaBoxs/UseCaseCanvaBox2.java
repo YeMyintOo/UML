acb14d95e351829ad07099bd05d6e733a732a694
Ye Myint Oo <dynamic.mdy.95@gmail.com>
2016-05-09 21:02:46 +0630

    GUI: Modify UseCase Actor Body Drawing method

--- src/CanvaBoxs/UseCaseCanvaBox2.java
+++ src/CanvaBoxs/UseCaseCanvaBox2.java
@@ -93,9 +93,9 @@ public class UseCaseCanvaBox2 extends Pane {
 					switch (toolHandler.getTool()) {
 					case "UseCase_Actor":
 						actor = new UC_Actor(key.getX(), key.getY(), 20, color, Color.GRAY);
-						drawActorBody(actor);// Body
 						isActor = true;
-						getChildren().add(actor);
+						getChildren().addAll(actor, actor.getBody(), actor.getLeg(), actor.getLeg2(), actor.getLeg3(),
+								actor.getLeg4(), actor.getLabel());
 						break;
 					case "UseCase_Action":
 						actionLine = new UC_ActionLine(key.getX(), key.getY(), key.getX(), key.getY(), color);
@@ -257,7 +257,7 @@ public class UseCaseCanvaBox2 extends Pane {
 							TextField data = new TextField();
 							data.layoutXProperty().bind(actors.get(index).centerXProperty().subtract(60));
 							data.layoutYProperty().bind(actors.get(index).centerYProperty().add(60));
-							//data.managedProperty().bind(data.visibleProperty());
+							// data.managedProperty().bind(data.visibleProperty());
 							getChildren().add(data);
 							data.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
 								@Override
@@ -385,51 +385,6 @@ public class UseCaseCanvaBox2 extends Pane {
 				}
 			}
 		}
-	}
-
-	public void drawActorBody(UC_Actor actor) {
-
-		double centerx = actor.getCenterX();
-		double centery = actor.getCenterY();
-		Text label = new Text(actor.labelProperty().getValue());
-		label.setFont(Font.font("Arial", FontWeight.BLACK, 16));
-
-		Line body = new Line(centerx, centery + 20, centerx, centery + 40);
-		body.startXProperty().bind(actor.centerXProperty());
-		body.startYProperty().bind(actor.centerYProperty().add(20));
-		body.endXProperty().bind(actor.centerXProperty());
-		body.endYProperty().bind(actor.centerYProperty().add(40));
-
-		Line leg = new Line(centerx, centery + 20, centerx + 10, centery + 20 + 10);
-		leg.startXProperty().bind(actor.centerXProperty());
-		leg.startYProperty().bind(actor.centerYProperty().add(20));
-		leg.endXProperty().bind(actor.centerXProperty().add(10));
-		leg.endYProperty().bind(actor.centerYProperty().add(30));
-
-		Line leg2 = new Line(centerx, centery + 20, centerx - 10, centery + 20 + 10);
-		leg2.startXProperty().bind(actor.centerXProperty());
-		leg2.startYProperty().bind(actor.centerYProperty().add(20));
-		leg2.endXProperty().bind(actor.centerXProperty().subtract(10));
-		leg2.endYProperty().bind(actor.centerYProperty().add(30));
-
-		Line leg3 = new Line(centerx, centery + 40, centerx + 10, centery + 40 + 10);
-		leg3.startXProperty().bind(actor.centerXProperty());
-		leg3.startYProperty().bind(actor.centerYProperty().add(40));
-		leg3.endXProperty().bind(actor.centerXProperty().add(10));
-		leg3.endYProperty().bind(actor.centerYProperty().add(50));
-
-		Line leg4 = new Line(centerx, centery + 40, centerx - 10, centery + 40 + 10);
-		leg4.startXProperty().bind(actor.centerXProperty());
-		leg4.startYProperty().bind(actor.centerYProperty().add(40));
-		leg4.endXProperty().bind(actor.centerXProperty().subtract(10));
-		leg4.endYProperty().bind(actor.centerYProperty().add(50));
-
-		label.textProperty().bind(actor.labelProperty());
-		label.layoutXProperty().bind(actor.centerXProperty().subtract(label.getLayoutBounds().getWidth() / 3));
-		label.layoutYProperty().bind(actor.centerYProperty().add(80));
-
-		getChildren().addAll(body, leg, leg2, leg3, leg4, label);
-
 	}
 
 	public void drawBoxLabel(UC_Box box) {
--- src/Canvas/UC_Actor.java
+++ src/Canvas/UC_Actor.java
@@ -15,6 +15,9 @@ public class UC_Actor extends Circle {
 	private Text label;
 	private Line body;
 	private Line leg;
+	private Line leg2;
+	private Line leg3;
+	private Line leg4;
 
 	public UC_Actor() {
 		super(0, 0, 0);
@@ -32,14 +35,39 @@ public class UC_Actor extends Circle {
 
 		label = new Text(data.get());
 		label.setFont(Font.font("Arial", FontWeight.BLACK, 16));
+		label.textProperty().bind(labelProperty());
+		label.layoutXProperty().bind(centerXProperty().subtract(label.getLayoutBounds().getWidth() / 2));
+		label.layoutYProperty().bind(centerYProperty().add(80));
 
 		body = new Line();
 		body.startXProperty().bind(centerXProperty());
 		body.startYProperty().bind(centerYProperty().add(20));
 		body.endXProperty().bind(centerXProperty());
 		body.endYProperty().bind(centerYProperty().add(40));
-		
-		
+
+		leg = new Line();
+		leg.startXProperty().bind(centerXProperty());
+		leg.startYProperty().bind(centerYProperty().add(20));
+		leg.endXProperty().bind(centerXProperty().add(10));
+		leg.endYProperty().bind(centerYProperty().add(30));
+
+		leg2 = new Line();
+		leg2.startXProperty().bind(centerXProperty());
+		leg2.startYProperty().bind(centerYProperty().add(20));
+		leg2.endXProperty().bind(centerXProperty().subtract(10));
+		leg2.endYProperty().bind(centerYProperty().add(30));
+
+		leg3 = new Line();
+		leg3.startXProperty().bind(centerXProperty());
+		leg3.startYProperty().bind(centerYProperty().add(40));
+		leg3.endXProperty().bind(centerXProperty().add(10));
+		leg3.endYProperty().bind(centerYProperty().add(50));
+
+		leg4 = new Line();
+		leg4.startXProperty().bind(centerXProperty());
+		leg4.startYProperty().bind(centerYProperty().add(40));
+		leg4.endXProperty().bind(centerXProperty().subtract(10));
+		leg4.endYProperty().bind(centerYProperty().add(50));
 	}
 
 	public final StringProperty labelProperty() {
@@ -52,5 +80,21 @@ public class UC_Actor extends Circle {
 
 	public Line getBody() {
 		return body;
+	}
+
+	public Line getLeg() {
+		return leg;
+	}
+
+	public Line getLeg2() {
+		return leg2;
+	}
+
+	public Line getLeg3() {
+		return leg3;
+	}
+
+	public Line getLeg4() {
+		return leg4;
 	}
 }
